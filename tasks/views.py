63d61d37c3d3af611bbccf8bae5388166de22180
ridwanul5 <ridwanulcse@gmail.com>
2020-08-08 22:45:53 +0600

    task add done

--- tasks/views.py
+++ tasks/views.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=4)
     # priority : another variable for forms 
 # Create your views here.
 tasks_py = ["foo", "boo", "baz"]
@@ -15,7 +15,18 @@ def index(request):
         "tasks_html": tasks_py
     })
 
-def add(request): 
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks_py.append(task) #task_py e add hobe (not tasks_html)
+
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+    
     return render(request, "tasks/add.html", {
-            "form": NewTaskForm()
+        "form": NewTaskForm()
     })
